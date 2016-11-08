cfa486f6d9877c98b6ed3b541b132437f1f3adb3
Juan Mendez <contact@juanmendez.info>
2016-11-07 20:36:40 -0600

    text shadow

--- app/src/main/java/info/juanmendez/md/stylesandthemes/MainActivity.java
+++ app/src/main/java/info/juanmendez/md/stylesandthemes/MainActivity.java
@@ -1,10 +1,25 @@
 package info.juanmendez.md.stylesandthemes;
 
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
+
+    @ViewById(R.id.textView5)
+    TextView txtGradient;
+
+    @AfterViews
+    void afterViews(){
+        LinearGradient lg = new LinearGradient(0, 0, txtGradient.getTextSize(), 0, Color.GREEN, Color.RED, Shader.TileMode.MIRROR );
+        txtGradient.getPaint().setShader( lg );
+    }
 }
