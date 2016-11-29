b725b5b4522df923608358a5161e3829e258752e
Juan Mendez <contact@juanmendez.info>
2016-11-29 06:01:25 -0600

    saving all content

--- app/src/main/java/info/juanmendez/md/befamiliar/app/MainActivity.java
+++ app/src/main/java/info/juanmendez/md/befamiliar/app/MainActivity.java
@@ -2,12 +2,13 @@ package info.juanmendez.md.befamiliar.app;
 
 import android.app.ActivityOptions;
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.LinearGradient;
-import android.graphics.Shader;
 import android.os.Build;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
@@ -17,19 +18,52 @@ import org.androidannotations.annotations.OptionsMenu;
 import org.androidannotations.annotations.ViewById;
 
 import info.juanmendez.md.befamiliar.R;
+import info.juanmendez.md.befamiliar.adapter.RecyclerAdapter;
+import info.juanmendez.md.befamiliar.service.LandscapeService;
 
 @EActivity(R.layout.activity_main)
 @OptionsMenu(R.menu.main_menu)
 public class MainActivity extends AppCompatActivity {
 
-    @ViewById(R.id.textView5)
-    TextView txtGradient;
+    @ViewById
+    Toolbar toolbar;
+
+    @ViewById
+    RecyclerView recyclerView;
+
+    @ViewById(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
 
     @AfterViews
     void afterViews(){
-        LinearGradient lg = new LinearGradient(0, 0, txtGradient.getTextSize(), 0, Color.GREEN, Color.RED, Shader.TileMode.MIRROR );
-        txtGradient.getPaint().setShader( lg );
+
+        setupToolbar();
+        setupDrawer();
+        setupRecyclerView();
     }
+
+    private void setupToolbar(){
+        setSupportActionBar( toolbar );
+        toolbar.setTitle("Welcome!");
+        toolbar.setSubtitle("Folks!");
+    }
+
+    private void setupRecyclerView(){
+        RecyclerAdapter adapter = new RecyclerAdapter(this, LandscapeService.getData());
+        recyclerView.setAdapter(adapter);
+
+        LinearLayoutManager mLinearLayoutManagerVertical = new LinearLayoutManager(this); // (Context context, int spanCount)
+        mLinearLayoutManagerVertical.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(mLinearLayoutManagerVertical);
+
+        recyclerView.setItemAnimator(new DefaultItemAnimator()); // Even if we dont use it then also our items shows default animation.
+    }
+
+    private void setupDrawer(){
+        NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(R.id.nav_drwr_fragment);
+        drawerFragment.setUpDrawer(drawerLayout, toolbar);
+    }
+
 
     @OptionsItem(R.id.menu_button)
     void onMenuButton(){
@@ -47,7 +81,5 @@ public class MainActivity extends AppCompatActivity {
         }else{
             startActivity( intent );
         }
-
-
     }
 }
--- app/src/main/java/info/juanmendez/md/befamiliar/app/NavigationDrawerFragment.java
+++ app/src/main/java/info/juanmendez/md/befamiliar/app/NavigationDrawerFragment.java
@@ -1,13 +1,72 @@
 package info.juanmendez.md.befamiliar.app;
 
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EFragment;
+import org.androidannotations.annotations.ViewById;
+
+import info.juanmendez.md.befamiliar.R;
+import info.juanmendez.md.befamiliar.adapter.NavigationDrawerAdapter;
+import info.juanmendez.md.befamiliar.service.LandscapeService;
 
 
 /**
- * Created by Juan Mendez on 11/9/2016.
+ * Created by Juan Mendez on 11/28/2016.
  * www.juanmendez.info
  * contact@juanmendez.info
  */
-
+@EFragment(R.layout.fragment_navigation_drawer)
 public class NavigationDrawerFragment extends Fragment {
+
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout            mDrawerLayout;
+
+    @ViewById(R.id.drawerList)
+    RecyclerView recyclerView;
+
+    @AfterViews
+    void afterViews(){
+        setUpRecyclerView();
+    }
+
+    private void setUpRecyclerView() {
+
+        NavigationDrawerAdapter adapter = new NavigationDrawerAdapter(getActivity(), LandscapeService.getDrawerData() );
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+    }
+
+    public void setUpDrawer(DrawerLayout drawerLayout, Toolbar toolbar) {
+        mDrawerLayout = drawerLayout;
+        mDrawerToggle = new ActionBarDrawerToggle(getActivity(), drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+//                getActivity().invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+//                getActivity().invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                super.onDrawerSlide(drawerView, slideOffset);
+                // Do something of Slide of Drawer
+            }
+        };
+
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
+
+        mDrawerLayout.post(() -> mDrawerToggle.syncState());
+    }
 }
